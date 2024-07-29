34f0d4e9457c0be792ae8d22689797cc39daf5cd
Vinícius Brevesteky Teixeira <130590509+CheerfulVini@users.noreply.github.com>
2024-07-29 13:34:50 -0300

    Update pokemons.php

--- pokemons.php
+++ pokemons.php
@@ -13,6 +13,7 @@ class pokemon{
     public $ataque;
     public $defesa;
     public $bonusDano = 0;
+    public $experiencia;
     public $nivel;
 
     public $temFolhada;
@@ -36,7 +37,12 @@ class pokemon{
         $this -> bonusDano = $this -> bonusDano + 10;
     }
 
-    function __construct($no, $nu, $ti, $se, $ta, $pe, $vi, $ve, $at, $de, $ni = 0, $fo = false, $af = false, $ar = false, $pm = false) {
+    function sobeNivel(){
+        $this -> nivel = $this -> nivel + 1;
+        $this -> experiencia = 0;
+    }
+
+    function __construct($no, $nu, $ti, $se, $ta, $pe, $vi, $ve, $at, $de, $xp = 0, $ni = 0, $fo = false, $af = false, $ar = false, $pm = false) {
         $this->nome = $no;
         $this->numero = $nu;
         $this->tipo = $ti;
@@ -47,6 +53,7 @@ class pokemon{
         $this->velocidade = $ve;
         $this->ataque = $at;
         $this->defesa = $de;
+        $this->experiencia = $xp;
         $this->nivel = $ni;
         $this->temFolhada = $fo;
         $this->temAfiarUnhas = $af;
@@ -55,5 +62,5 @@ class pokemon{
     }
 }
 
-$sprigatito = new pokemon("Sprigatito", 906, "Grama", "Fêmea", 0.50, 4.1, 1, 40, 65, 61, 54, true, true);
-$pikachu = new pokemon("Pikachu", 25, "Elétrico", "Macho", 0.45, 6, 1, 35, 90, 55, 40, false, false, true, true);
+$sprigatito = new pokemon("Sprigatito", 906, "Grama", "Fêmea", 0.50, 4.1, 0, 1, 40, 65, 61, 54, true, true);
+$pikachu = new pokemon("Pikachu", 25, "Elétrico", "Macho", 0.45, 6, 0, 1, 35, 90, 55, 40, false, false, true, true);
