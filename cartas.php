6b02ff0936a5f9174d9d4f7f4726acfb44e9d0cc
Vinícius Brevesteky Teixeira <130590509+CheerfulVini@users.noreply.github.com>
2024-09-12 15:45:59 -0300

    Update cartas.php

--- cartas.php
+++ cartas.php
@@ -5,12 +5,14 @@ class Carta{
    private $custoMana;
    private $tipo;
    private $valor;
+   private $num;
 
-   function __construct($no, $cm, $ti, $va){
+   function __construct($no, $cm, $ti, $va, $nu){
         $this->nome = $no;
         $this->custoMana = $cm;
         $this->tipo = $ti;
         $this->valor = $va;
+        $this->num = $nu;
    }
 
    
@@ -86,12 +88,67 @@ class Carta{
 
       return $this;
    }
+   
+   public function getNum()
+   {
+      return $this->num;
+   }
+
+   /**
+    * Set the value of valor
+    */
+   public function setNum($num): self
+   {
+      $this->num = $num;
+
+      return $this;
+   }
 }
 
-$blackLotus = new Carta("Lótus Negra", "0", "artefato", 500000);
-$atraxa = new Carta("Atraxa, Grande Unificadora", "3gwub", "criatura lendária - pretor", 130);
-$nadu = new Carta("Nadu, Sabedoria Alada", "1gu", "criatura lendária - ave mago", 14);
-$sheoldred = new Carta("Sheoldred, o Apocalipse", "2bb", "criatura lendária - phyrexiano pretos" , 430);
-$estudoRistico = new Carta("Estudo Rístico", "2u", "encantamento", 219);
-$path = new Carta("Caminho para o Exílio", "w", "mágica instantânea", 5);
-$solRing = new Carta("Anel Solar", "1", "artefato", 5);
+$cartas[0] = new Carta("Lótus Negra", "0", "artefato", 500000, 1);
+$cartas[1] = new Carta("Atraxa, Grande Unificadora", "3gwub", "criatura lendária - pretor", 130, 2);
+$cartas[2] = new Carta("Nadu, Sabedoria Alada", "1gu", "criatura lendária - ave mago", 14, 3);
+$cartas[3] = new Carta("Sheoldred, o Apocalipse", "2bb", "criatura lendária - phyrexiano pretos" , 430, 4);
+$cartas[4] = new Carta("Estudo Rístico", "2u", "encantamento", 219, 5);
+$cartas[5] = new Carta("Caminho para o Exílio", "w", "mágica instantânea", 5, 6);
+$cartas[6] = new Carta("Anel Solar", "1", "artefato", 5, 7);
+$cartas[7] = new Carta("Ato Blásfemo", "8r", "feitiço", 10, 8);
+
+$carta_sorteada = $cartas[array_rand($cartas)];
+$adivinha = 0;
+$tentativas = 0;
+
+while($adivinha != $carta_sorteada->getNome()){
+    $tentativas++;
+    print("\nAdivinhe o nome(digite 0 para sair):");
+    $adivinha = readline();
+
+    if($carta_sorteada->getNome() == $adivinha){
+        print("\nVocê acertou! sua pontuação foi de: " . 100/$tentativas);
+    }else if($adivinha == 0){
+        die();
+    }else if($carta_sorteada->getNome() != $adivinha){
+        print("\nVocẽ errou.");
+
+        $adivinha = rand(1, 2);
+
+        if($adivinha == 1){
+            print("\nSua dica é: ");
+            $adivinha = rand(1, 3);
+
+            switch ($adivinha) {
+                case 1:
+                    print("\nO custo de mana é: " . $carta_sorteada->getNome());
+                break;
+
+                case 2:
+                    print("\nO tipo é: " . $carta_sorteada->getTipo());
+                break;
+
+                case 3:
+                    print("\nO preço na plataforma LigaMagic é: " . $carta_sorteada->getValor());
+                break;
+            }
+        }
+    }
+}
